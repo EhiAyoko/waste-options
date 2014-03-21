0099b4b7e559c5a0825f53db7a6be0f1cab5caa0
Ehi Aimiuwu <eaimiuwu@icstars.org>
2014-03-21 15:39:54 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WasteOptions::Application.config.secret_token = '921aa1c4427668a37b4c9c44fb5e328431190117a67e1babedb3417d0c5144a9c08e77eed5e70b01d521988e721dd69f6b77fe1b11534480707577b51b39dd9a'
